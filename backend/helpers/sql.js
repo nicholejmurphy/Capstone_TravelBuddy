aea0acc1232c3143a52297a65d08fdd374472b81
Nichole Murphy <nicholejmurphy@gmail.com>
2023-10-14 17:45:47 -0400

    Created SQL helper for translating js to SQL formatted queries

---
+++ backend/helpers/sql.js
@@ -0,0 +1,35 @@
+const { BadRequestError } = require("../expressError");
+
+/**
+ * Helper for making selective update queries.
+ *
+ * The calling function can use it to make the SET clause of an SQL UPDATE
+ * statement.
+ *
+ * @param dataToUpdate {Object} {field1: newVal, field2: newVal, ...}
+ * @param jsToSql {Object} maps js-style data fields to database column names,
+ *   like { firstName: "first_name", lastName: "last_name" }
+ *
+ * @returns {Object} {sqlSetCols, dataToUpdate}
+ *
+ * @example {firstName: 'Aliya', lastName: 'Smith'} =>
+ *   { setCols: '"first_name"=$1, "last_name"=$2',
+ *     values: ['Aliya', 'Smith'] }
+ */
+
+function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+  const keys = Object.keys(dataToUpdate);
+  if (keys.length === 0) throw new BadRequestError("No data");
+
+  // {firstName: 'Aliya', lastName: 'Smith'} => ['"first_name"=$1', '"last_name"=$2']
+  const cols = keys.map(
+    (colName, idx) => `"${jsToSql[colName] || colName}"=$${idx + 1}`
+  );
+
+  return {
+    setCols: cols.join(", "),
+    values: Object.values(dataToUpdate),
+  };
+}
+
+module.exports = { sqlForPartialUpdate };
