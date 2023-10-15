6d3c50422677288310fb844c49066b6ef26c85bf
Nichole Murphy <nicholejmurphy@gmail.com>
2023-10-15 15:08:19 -0400

    Created auth routes with testing

---
+++ backend/routes/auth.js
@@ -0,0 +1,62 @@
+"use strict";
+
+/** Routes for authentication. */
+
+const jsonschema = require("jsonschema");
+
+const User = require("../models/user");
+const express = require("express");
+const router = new express.Router();
+const { createToken } = require("../helpers/tokens");
+const userAuthSchema = require("../schemas/userAuth.json");
+const userSignupSchema = require("../schemas/userSignup.json");
+const { BadRequestError } = require("../expressError");
+
+/** POST /auth/token:  { username, password } => { token }
+ *
+ * Returns JWT token which can be used to authenticate further requests.
+ *
+ */
+
+router.post("/token", async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, userAuthSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map((e) => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const { username, password } = req.body;
+    const user = await User.authenticate(username, password);
+    const token = createToken(user);
+    return res.json({ token });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** POST /auth/signup:   { user } => { token }
+ *
+ * user must include { username, password, firstName, lastName }
+ *
+ * Returns JWT token which can be used to authenticate further requests.
+ *
+ */
+
+router.post("/signup", async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, userSignupSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map((e) => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const newUser = await User.signup({ ...req.body });
+    const token = createToken(newUser);
+    return res.status(201).json({ token });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
