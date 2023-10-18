eef4f7d70bf121c67ca3b87430fd195e662bcccc
Nichole Murphy <nicholejmurphy@gmail.com>
2023-10-18 14:42:45 -0400

    Restructured components and simplified homepage.

--- frontend/src/homepage/Homepage.js
+++ frontend/src/homepage/Homepage.js
@@ -2,38 +2,45 @@ import React, { useContext } from "react";
 // import "./Homepage.css";
 import {
   Button,
-  CardBody,
-  // CardSubtitle,
-  CardText,
-  // CardTitle,
+  //   CardBody,
+  //   CardSubtitle,
+  //   CardText,
+  //   CardTitle,
 } from "reactstrap";
 import UserContext from "../auth/UserContext";
+
+// updated default user location with TravelApi.update({ locationId, locationName })
 
 function Homepage() {
   const { currUser } = useContext(UserContext);
 
+  const [locations, setLocations] = useState(null);
+
+  async function searchFor(name) {
+    let res = await TravelApi.searchLocation(name, (category = "geos"));
+    setLocations(res);
+  }
+
   function isLoggedIn() {
     return (
-      <CardBody className="Homepage bg-light shadow rounded">
-        <CardText className="Homepage-welcome display-4">
-          Welcome back, {currUser.firstName}!
-        </CardText>
-      </CardBody>
+      <div>
+        <h3>Welcome back, {currUser.firstName}!</h3>
+      </div>
     );
   }
 
   function notLoggedIn() {
     return (
-      <CardBody className="Homepage bg-light">
-        <CardText className="Homepage-btns text-center">
-          <Button className="bg-primary" tag="a" href="/login">
-            Login
-          </Button>
-          <Button className="bg-primary" tag="a" href="/signup">
-            SignUp
-          </Button>
-        </CardText>
-      </CardBody>
+      <div>
+        <h3>Welcome to TravelBuddy!</h3>
+        <p>Login or Signup to get started.</p>
+        <Button className="bg-primary" tag="a" href="/login">
+          Login
+        </Button>
+        <Button className="bg-primary" tag="a" href="/signup">
+          SignUp
+        </Button>
+      </div>
     );
   }
 
