2a8a50adb4fcfc7acfb93582117a4824763ec291
Nichole Murphy <nicholejmurphy@gmail.com>
2023-10-19 18:11:09 -0400

    Updated response data depth on routes

--- frontend/src/api/travelApi.js
+++ frontend/src/api/travelApi.js
@@ -1,17 +1,15 @@
 import axios from "axios";
-import { TRAVEL_API_KEY } from "../keys";
 
-const BASE_URL = "https://api.content.tripadvisor.com/api/v1/location";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
-/** API Class.
+/** API Class for TripAdvisor API calls from backend.
  *
- * Static class tying together methods used to get/send to to the TripAdvisor API.
+ * Static class tying together methods used to send requests to
+ * the backend server which calls to the TripAdvisor API.
  *
  */
 class TravelApi {
   static async request(endpoint, data = {}, method = "get") {
-    console.debug("API Call:", endpoint, data, method);
-
     // passing authorization token through the header
     const url = `${BASE_URL}/${endpoint}`;
     const headers = { accept: "application/json" };
@@ -28,44 +26,37 @@ class TravelApi {
 
   // Individual API routes
 
-  /** Location Search. { name, category } =>
+  /** Location Search. { searchTerm, category } =>
    *    { [{ location1 }, {location2 }] }
    *
    * category options: { geos, attractions, restaurants, hotels }
    * >>> TIP: For a general search, use category: geos to yield
    *     general geographical locations
    */
-  static async searchLocation(name, category) {
-    // Update name to be URL encoded if a space existis
-    name = name.trim().replace(/\s+/g, "%20");
-    let res = await this.request(
-      `search?key=${TRAVEL_API_KEY}&searchQuery=${name}%20nj&category=${category}&language=en`
-    );
-    return res.data;
+  static async searchLocation(searchTerm, category) {
+    // Update searchTerm to be URL encoded if a space existis
+    searchTerm = searchTerm.trim().replace(/\s+/g, "%20").replace(/,/g, "%2C");
+    let res = await this.request(`locations/search/${searchTerm}/${category}`);
+    const locations = res.locations.data;
+    return locations;
   }
 
   /** Location Details. { locationId } => { locationDetails } */
   static async getLocationDetails(locationId) {
-    let res = await this.request(
-      `${locationId}/details?key=${TRAVEL_API_KEY}&language=en&currency=USD`
-    );
+    let res = await this.request(`locations/details/${locationId}`);
     return res;
   }
 
   /** Location Photos. { locationId } => { locationPhotos } */
   static async getLocationPhotos(locationId) {
-    let res = await this.request(
-      `${locationId}/photos?key=${TRAVEL_API_KEY}&language=en`
-    );
-    return res.data;
+    let res = await this.request(`locations/photos/${locationId}`);
+    return res.photos.data;
   }
 
   /** Location Reviews. { locationId } => { locationReviews } */
   static async getLocationReviews(locationId) {
-    let res = await this.request(
-      `${locationId}/reviews?key=${TRAVEL_API_KEY}&language=en`
-    );
-    return res.data;
+    let res = await this.request(`locations/reviews/${locationId}`);
+    return res.reviews.data;
   }
 }
 
