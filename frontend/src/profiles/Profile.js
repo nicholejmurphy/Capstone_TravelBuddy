4dc42d89ca95627fd7458adaff553e307260aa12
Nichole Murphy <nicholejmurphy@gmail.com>
2023-10-26 13:17:28 -0400

    Updated components for profile and showing saved locations

--- frontend/src/profiles/Profile.js
+++ frontend/src/profiles/Profile.js
@@ -3,26 +3,31 @@ import ProfileForm from "./ProfileForm";
 import UserContext from "../auth/UserContext";
 import TravelApi from "../api/travelApi";
 import LocationList from "../locations/LocationList";
+import Loading from "../common/Loading";
 
 /** Shows user 's profile data
  *  - Gets saved on data and passes to LocationList
  */
 function Profile({ logout }) {
   const { savedLocationIds } = useContext(UserContext);
-  const [locations, setLocations] = useState(null);
+  const [locations, setLocations] = useState([]);
+  const [dataIsLoading, setDataIsLoading] = useState(false);
 
   useEffect(() => {
     async function getLocationsOnMount() {
+      setDataIsLoading(true);
       const promiseArray = [];
       savedLocationIds.forEach((id) => {
         promiseArray.push(TravelApi.getLocationDetails(id));
       });
       let res = await Promise.all(promiseArray);
       setLocations(res);
+      setDataIsLoading(false);
     }
     getLocationsOnMount();
   }, [savedLocationIds]);
 
+  if (dataIsLoading) return <Loading />;
   return (
     <div className="Profile bg-light p-4 shadow rounded">
       <div className="m-3">
