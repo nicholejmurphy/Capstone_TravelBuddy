2bf80305da781aa7d3fba2cb8d6803f10b433ca0
Nichole Murphy <nicholejmurphy@gmail.com>
2023-10-27 09:59:27 -0400

    Streamlined route names for saved locations

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const { NotFoundError } = require("./expressError");
 const { authenticateJWT } = require("./middleware/auth");
 const authRoutes = require("./routes/auth");
 const usersRoutes = require("./routes/users");
-const locationRoutes = require("./routes/savedLocations");
+const savedLocations = require("./routes/savedLocations");
 const travelApiRoutes = require("./routes/travelApi");
 
 const morgan = require("morgan");
@@ -24,7 +24,7 @@ app.use(authenticateJWT);
 
 app.use("/auth", authRoutes);
 app.use("/users", usersRoutes);
-app.use("/savedLocations", locationRoutes);
+app.use("/savedLocations", savedLocations);
 app.use("/locations", travelApiRoutes);
 
 /** Handle 404 errors -- this matches everything */
