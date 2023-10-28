9f27e0d7217705b90ae936195f19ac16225c0fd0
Nichole Murphy <nicholejmurphy@gmail.com>
2023-10-28 09:28:10 -0400

    Added a trivia api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-// import "./App.css";
 import useLocalStorage from "./hooks/useLocalStorage";
 import UserApi from "./api/userApi";
 import { decodeToken } from "react-jwt";
---
+++ frontend/src/api/triviaApi.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+
+/** API Class for API Ninjas API calls from backend.
+ *
+ * Static class tying together methods used to send requests to
+ * the backend server which calls to the Ninjas API.
+ *
+ */
+class TriviaApi {
+  static async request(endpoint, data = {}, method = "get") {
+    // passing authorization token through the header
+    const url = `${BASE_URL}/${endpoint}`;
+    const headers = { accept: "application/json" };
+    const params = method === "get" ? data : {};
+
+    try {
+      return (await axios({ url, method, data, params, headers })).data;
+    } catch (err) {
+      console.error("API Error:", err.response);
+      let message = err.response.data.error.message;
+      throw Array.isArray(message) ? message : [message];
+    }
+  }
+
+  // Individual API routes
+
+  /** Get a random trivia question and answer
+   *
+   * - Sends request to backend which triggers and API
+   *   call to API Ninjas trivia endpoint
+   */
+  static async get() {
+    let res = await this.request(`trivia/`);
+    return res;
+  }
+}
+
+export default TriviaApi;
