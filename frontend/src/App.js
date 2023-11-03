7566d6e48d2f251b904ab60ab65c8a8ffb24ff99
Nichole Murphy <nicholejmurphy@gmail.com>
2023-11-03 14:43:05 -0400

    Removed loading comp. and added backdrop spinner

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,12 @@ import {
   ThemeProvider,
   createTheme,
 } from "@material-ui/core/styles";
-
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import Grid from "@material-ui/core/Grid";
 
 import UserApi from "./api/userApi";
 import UserContext from "./auth/UserContext";
-import Loading from "./common/Loading";
 import Navigation from "./navigation_routes/Navigation";
 import Routes from "./navigation_routes/Routes";
 
@@ -41,6 +41,10 @@ const useStyles = makeStyles((theme) => ({
     paddingBottom: "106px",
     height: "100%",
     flexGrow: 1,
+  },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff",
   },
 }));
 
@@ -150,7 +154,11 @@ function App() {
 
   // Show loading component if data is still loading.
   if (dataIsLoading) {
-    return <Loading />;
+    return (
+      <Backdrop className={classes.backdrop} open={dataIsLoading}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    );
   }
 
   return (
