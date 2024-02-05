66824ec238f209cf2dc636c504c5331e02500826
Nichole Murphy <nicholejmurphy@gmail.com>
2024-02-05 16:04:23 -0500

    Created react component for currency conversion

--- frontend/src/currencyConversion/Conversions.js
+++ frontend/src/currencyConversion/Conversions.js
@@ -1,13 +1,16 @@
-import React, { useContext, useState, useEffect } from "react";
-import ConversionApi from "../api/conversionApi";
-import ConversionForm from "./ConversionForm";
-import ConversionResults from "./ConversionResults";
+import React, { useState } from "react";
 
-import { Grid, Paper } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
 import Button from "@material-ui/core/Button";
+import Alert from "@material-ui/lab/Alert";
+import { makeStyles } from "@material-ui/core";
+
+import ConversionApi from "../api/conversionApi";
+import currencies from "./currencyList";
 
 const useStyles = makeStyles((theme) => ({
   title: {
@@ -16,66 +19,190 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: 200,
     color: "#ffffff",
   },
-  clear: { marginTop: "5px" },
-  response: {
+  body: {
     display: "flex",
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
-    flexDirection: "column",
+    flexDirection: "row",
     padding: "30px",
-    marginTop: "auto",
-    width: "90%",
+    margin: "auto",
+    marginTop: "30px",
+    width: "300px",
     backgroundColor: "rgba(255, 255, 255, 0.9)",
   },
-  body: {
-    paddingTop: "10px",
-    flexGrow: 1,
-  },
-  media: {
-    height: 0,
-    paddingTop: "56.25%", // 16:9
-  },
+  alert: { marginTop: "10px", maxWidth: "415px" },
+  submit: { width: "95%" },
+  result: { fontWeight: 700, fontSize: "1.5rem" },
 }));
 
-/** Shows User's saved locations (favorites) data
- *  - Gets saved data and passes to LocationList
+/** Currency Conversion Calculator
+ *  Takes a currency type and an amount to calculate value in another currency
+ *  Amount must be greater than 0
  *
- * { Further Study: }
- * Adding a drop down for user's saved locations
- * and will add saved translations/conversions here or on separate pages
  */
 function Conversions() {
   const classes = useStyles();
-  const [formData, setFormData] = useState(searchTerm);
+  const DEFAULT_DATA = {
+    to: "EUR",
+    from: "USD",
+    amount: 0,
+  };
+  const [formErrors, setFormErrors] = useState(null);
+  const [formData, setFormData] = useState(DEFAULT_DATA);
+  const [conversionResult, setConversionResult] = useState(null);
 
-  // Updates serchterm state on form input change
-  function handleChange(e) {
-    setFormData(e.target.value);
-    setError(false);
+  // On change of form data, the formData state is updated
+  async function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData((data) => ({
+      ...data,
+      [name]: value,
+    }));
   }
-
+  // On submission of form, data is sent to conversion API and state for conversionResult is updated
+  // Reveals error is error is thrown by API
   async function handleSubmit(e) {
     e.preventDefault();
-    if (!formData) {
-      setError(true);
-    } else {
-      setSearchTerm(formData);
+    try {
+      const res = await ConversionApi.convert(formData);
+      if (res.success) {
+        setFormErrors(null);
+        setConversionResult({
+          amount: formData.amount,
+          from: currencies[formData.from],
+          result: res.result,
+          to: currencies[formData.to],
+        });
+      } else {
+        setFormErrors(res.error.info);
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
+
   return (
     <Grid>
       <Typography variant="h4" className={classes.title} align="center">
         Currency Converter
       </Typography>
 
-      <Grid container justifyContent="center">
-        <ConversionForm></ConversionForm>
-        <ConversionResult></ConversionResult>
-        <Paper className={classes.response}>
-          <Typography variant="p">
-            Saved if needed as a styled container
-          </Typography>
+      <Grid container justifyContent="center" direction="column">
+        <Paper
+          className={classes.body}
+          component="form"
+          onSubmit={handleSubmit}
+        >
+          <Grid
+            container
+            justifyContent="center"
+            direction="column"
+            spacing={2}
+          >
+            <Grid container item justifyContent="center">
+              <Typography variant="caption">
+                Choose your currency conversions and an amount.
+              </Typography>
+            </Grid>
+            <Grid
+              container
+              item
+              justifyContent="center"
+              direction="row"
+              spacing={4}
+            >
+              <Grid item xs={3}>
+                <TextField
+                  id="currency-from"
+                  name="from"
+                  select
+                  label="From"
+                  value={formData.from}
+                  onChange={handleChange}
+                  variant="outlined"
+                >
+                  {Object.keys(currencies).map((option) => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </Grid>
+              <Grid item xs={8}>
+                <TextField
+                  id="currency-amount"
+                  name="amount"
+                  type="number"
+                  InputProps={{ inputProps: { min: 0.01, step: ".01" } }}
+                  label="Amount"
+                  value={formData.amount}
+                  onChange={handleChange}
+                  variant="outlined"
+                ></TextField>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              item
+              justifyContent="center"
+              alignItems="center"
+              spacing={4}
+              direction="row"
+            >
+              <Grid item xs={3}>
+                <TextField
+                  id="currency-to"
+                  name="to"
+                  select
+                  label="To"
+                  value={formData.to}
+                  onChange={handleChange}
+                  variant="outlined"
+                >
+                  {Object.keys(currencies).map((option) => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </Grid>
+              <Grid item container justifyContent="center" xs={8}>
+                <Button
+                  color="secondary"
+                  variant="contained"
+                  type="submit"
+                  size="large"
+                  className={classes.submit}
+                >
+                  Convert
+                </Button>
+              </Grid>
+            </Grid>
+            {formErrors && (
+              <Alert severity="error" className={classes.alert}>
+                {formErrors}
+              </Alert>
+            )}
+          </Grid>
         </Paper>
+        {conversionResult && (
+          <Paper className={classes.body}>
+            <Grid container direction="column">
+              <Grid item>
+                <Typography variant="subtitle1">
+                  {conversionResult.amount} {conversionResult.from}
+                  {conversionResult.amount > 1 ? "s" : null} is equal to
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body1" className={classes.result}>
+                  {conversionResult.result} {conversionResult.to}
+                  {conversionResult.result > 1 ? "s" : null}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Paper>
+        )}
       </Grid>
     </Grid>
   );
